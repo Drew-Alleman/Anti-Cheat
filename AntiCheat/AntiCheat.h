915bfdc3d69ced672dcbc1645bdf07f5303ebf51
Drew qsar <drew.allemann@gmail.com>
2021-07-06 06:46:53 -0700

    ReAdded functions from a1f1543
    Fixed merge conflicts

--- AntiCheat/AntiCheat.h
+++ AntiCheat/AntiCheat.h
@@ -3,9 +3,15 @@
 #include <string>
 #include <regex>
 
-std::vector<std::wstring> windowTitles;
+// Utils
+std::string toString(const std::wstring& wstr);
 
-std::string to_string(const std::wstring& wstr);
+//Enum Process Checks
 BOOL CALLBACK EnumWindowsProc(HWND hWnd, LPARAM lParam);
 bool regexWindowSearch(std::regex pattern);
 bool isWindowPatternFound(std::regex pattern);
+bool isWindowPatternFound(std::regex pattern);
+
+// Debugging Checks
+bool isDebuggerPresent();
+bool isRemoteDebuggerPresent();
