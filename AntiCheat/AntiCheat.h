07a191d87ede4865b5f6408adefed17d8d3b1cfc
Drew <drew.allemann@gmail.com>
2021-07-07 14:46:05 -0700

    Added Function Declaration Summary

--- AntiCheat/AntiCheat.h
+++ AntiCheat/AntiCheat.h
@@ -3,18 +3,30 @@
 #include <string>
 #include <regex>
 
-// Utils
+/* Converts a wstring into a std::string */
 std::string toString(const std::wstring& wstr);
 
-//Enum Process Checks
+/* Callback function for EnumWindows() */
 BOOL CALLBACK EnumWindowsProc(HWND hWnd, LPARAM lParam);
+
+/*
+returns true if the regex pattern matches the window title
+This function is called in isWindowPatternFound()
+ */
 bool regexWindowSearch(std::regex pattern);
-bool isWindowPatternFound(std::regex pattern);
+
+/*
+Gathers all windows titles opened and applies regex pattern to each one  
+returns true if the regex pattern matches a window title
+ */
 bool isWindowPatternFound(std::regex pattern);
 
 // Debugging Checks
 namespace Debugger {
+	/* Calls isDebuggerPresent(); */
 	bool isDebuggerPresent();
+	/* Can detect ring-3 debugger  */
 	bool isRemoteDebuggerPresent();
+	/* Calls isDebuggerPresent(), and isRemoteDebuggerPresent() and returns true if debugger is found */
 	bool Check();
 }
